ff1e708f938f54d509ef4c24c0e59f5467bef0b9
illakaya <mimijane.nguyen@gmail.com>
2024-03-25 23:08:57 +1100

    re-added blog.js file, created the initialise function that renders each blog post

---
+++ assets/js/blog.js
@@ -0,0 +1,44 @@
+// Grab all the references to the DOM elements
+const blogPost = document.querySelector('#blog');
+
+// we need to store each blog post as an object in an array
+let blogStorageObject = [];
+
+function renderBlogPosts() {
+    const storedBlogPosts = JSON.parse(localStorage.getItem(`blogPosts`));
+    if (!storedBlogPosts) {
+        return;
+    }
+    blogPost.textcontent = ``;
+    for (let i = 0; i < storedBlogPosts.length; i++) {
+        const post = storedBlogPosts[i];
+        const title = storedBlogPosts[i].title;
+        const content = storedBlogPosts[i].content;
+        const user = storedBlogPosts[i].username;
+
+        const div = document.createElement('div');
+        // div.textContent = JSON.stringify(post);
+        
+        const h1 = document.createElement(`h1`);
+        const p = document.createElement('p');
+        const span = document.createElement(`span`);
+        h1.textContent = title;
+        p.textContent = content;
+        span.textContent = `Posted by: ` + user;
+
+        div.appendChild(h1);
+        div.appendChild(p);
+        div.appendChild(span);
+        blogPost.appendChild(div);
+    }
+};
+
+function init() {
+    const storeBlogs = JSON.parse(localStorage.getItem(`blogPosts`));
+    if(storeBlogs !== null) {
+        blogStorageObject = storeBlogs;
+    }
+    renderBlogPosts();
+}
+
+init();
